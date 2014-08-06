6a8105cc8be5aaae30926e5631ea55238b434fe9
prexlebre <prexlebre@gmail.com>
2014-08-07 01:33:30 +0200

    Better fibonacci numbers.
    Added dependency for Data.Matrix for helper.hs

--- helper.hs
+++ helper.hs
@@ -5,11 +5,14 @@ module Helper
 , digits	
 ) where 
 
+-- http://hackage.haskell.org/package/matrix-0.3.3.0/docs/Data-Matrix.html
+import qualified Data.Matrix as M
 
-fib :: (Integral a) => a -> a
-fib 1 = 1
-fib 2 = 1
-fib n = fib  (n-1) + fib (n-2)
+fib :: (Num a) => Int -> a
+fib n = M.getElem 1 2 $ powerMatrix n
+	where
+		powerMatrix n = foldl1 M.multStd2 $ replicate n matrix
+		matrix = M.fromList 2 2 [1,1,1,0]
 
 primes :: (Integral a) => [a]
 primes = sieve [2..]
