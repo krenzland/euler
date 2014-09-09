0ee505a6426d09b7cb8831097e185bf678da217b
Lukas Krenz <lukas@krenz.land>
2014-09-09 20:44:48 +0200

    Euler 99. A lot faster.

--- euler099.hs
+++ euler099.hs
@@ -3,15 +3,16 @@ import qualified Data.Text as T
 main :: IO ()
 main = euler99
 
+euler99 :: IO()
 euler99 = readFile file >>= print . getLineNumber . reduce . parse  
   where
     file = "p099_base_exp.txt"
     getLineNumber n = snd . maximum $ zip n [1..]
 
-reduce :: [[Integer]] -> [Integer]
-reduce = fmap (\x -> (x !! 0) ^ (x !! 1)) -- or n^(log e)
-  
--- GOD, THIS SHIT IS SO FUCKING UGLY! THIS CAN'T BE! OH NO!
+reduce :: [[Integer]] -> [Float]
+reduce = fmap (\x -> (ilog (x !! 0)) * (fromIntegral (x !! 1)))
+  where
+    ilog n = log $ fromIntegral n 
 
 parse n = fmap (fmap read) $ fmap (split) $ lines n :: [[Integer]] 
 
