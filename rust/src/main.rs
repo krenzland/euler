99af2d6fd476748e57f4d38920b8cd1b5235d552
Lukas Krenz <lukas@krenz.land>
2017-03-03 14:05:27 +0100

    Add clap to rust.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,28 @@
+extern crate clap;
+use clap::{Arg, App};
+
+mod p11;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Rust-Euler")
+        .version("42.0")
+        .author("Lukas K.")
+        .about("Solves Project Euler solutions in Rust.")
+        .arg(Arg::with_name("problem")
+            .short("p")
+            .long("problem")
+            .takes_value(true)
+            .required(false))
+        .get_matches();
+
+    if let Some(problem) = matches.value_of("problem") {
+        match problem {
+            "11" => p11::main(),
+            _ => println!("Unknown problem id!"),
+        }
+    } else {
+        p11::main();
+        // Run most recent problem.
+    }
+    
 }
