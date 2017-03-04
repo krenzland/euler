8c9317541883c6f8f810291056fb31db4dad82ed
Lukas Krenz <lukas@krenz.land>
2017-03-04 18:10:47 +0100

    Format stuff.

--- rust/src/p33.rs
+++ rust/src/p33.rs
@@ -18,7 +18,9 @@ fn is_curious_frac(nom: u32, denom: u32) -> bool {
     let mut intersec = nom_digs.intersection(&denom_digs);
     // Contains 0 -> divisible by 10, |intersec| = 0 -> nothing to cancel
     // |intersec| = 2 -> trivial
-    if intersec.clone().count() != 1 || intersec.any(|&x| x == 0){ return false; }
+    if intersec.clone().count() != 1 || intersec.any(|&x| x == 0) {
+        return false;
+    }
     let nom_dig = nom_digs.iter().find(|x| !denom_digs.contains(x));
     let denom_dig = denom_digs.iter().find(|x| !nom_digs.contains(x));
     if let (Some(&nom_dig), Some(&denom_dig)) = (nom_dig, denom_dig) {
@@ -32,10 +34,12 @@ fn is_curious_frac(nom: u32, denom: u32) -> bool {
 }
 
 pub fn p33() -> u32 {
-    let mut prod = Rational::new(1,1);
+    let mut prod = Rational::new(1, 1);
     for nom in 10..100 {
         for denom in 10..100 {
-            if (nom as f32)/(denom as f32) > 1.0 { continue; }
+            if (nom as f32) / (denom as f32) > 1.0 {
+                continue;
+            }
             if is_curious_frac(nom, denom) {
                 let ratio = Rational::new(nom as isize, denom as isize);
                 prod = prod * ratio;
@@ -51,13 +55,13 @@ pub fn main() {
 
 #[test]
 fn test_is_curious_frac() {
-    assert!(is_curious_frac(49,98));
-    assert!(is_curious_frac(16,64));
-    assert!(is_curious_frac(19,95));
-    assert!(is_curious_frac(49,98));
-    assert!(!is_curious_frac(49,89));
-    assert!(!is_curious_frac(30,50));
-    assert!(!is_curious_frac(12,36));
+    assert!(is_curious_frac(49, 98));
+    assert!(is_curious_frac(16, 64));
+    assert!(is_curious_frac(19, 95));
+    assert!(is_curious_frac(49, 98));
+    assert!(!is_curious_frac(49, 89));
+    assert!(!is_curious_frac(30, 50));
+    assert!(!is_curious_frac(12, 36));
 }
 
 #[test]
--- rust/src/p65.rs
+++ rust/src/p65.rs
@@ -1,4 +1,4 @@
-//https://projecteuler.net/problem=65
+// https://projecteuler.net/problem=65
 
 use num::bigint::BigInt;
 use num::FromPrimitive;
@@ -24,26 +24,27 @@ fn digit_sum_str(x: &str) -> u32 {
 }
 
 fn sum_of_nth_nominator(num_conv: usize) -> u32 {
-    let even = (1..num_conv/2).map(|i| i*2);
+    let even = (1..num_conv / 2).map(|i| i * 2);
     let mut series: Vec<isize> = vec![1];
     for e in even {
         let e = e as isize;
-        series.extend(&[e,1,1]);
+        series.extend(&[e, 1, 1]);
     }
-    series.resize(num_conv-1, 0);
+    series.resize(num_conv - 1, 0);
 
     // The nominators follow this formula:
     // h_0 = a_0
     // h_1 = a_1 * a_0 + 1
     // h_n = a_n * h_{n_1} + h_{n-2}
-    let mut nominators = vec![BigInt::from_isize(2).unwrap(), BigInt::from_isize(2*series[0] + 1).unwrap()];
+    let mut nominators = vec![BigInt::from_isize(2).unwrap(),
+                              BigInt::from_isize(2 * series[0] + 1).unwrap()];
     for (i, &coeff) in series.iter().skip(1).enumerate() {
         let i = i + 2;
         let coeff = BigInt::from_isize(coeff).unwrap();
-        let nom = coeff * nominators[i-1].clone() + nominators[i-2].clone();
+        let nom = coeff * nominators[i - 1].clone() + nominators[i - 2].clone();
         nominators.push(nom);
     }
-    let digits = nominators[num_conv-1].to_str_radix(10);
+    let digits = nominators[num_conv - 1].to_str_radix(10);
     digit_sum_str(&digits)
 }
 
@@ -73,7 +74,7 @@ fn test_digit_sum_str() {
 
 #[test]
 fn test() {
-    assert_eq!(sum_of_nth_nominator(10),17);
-    assert_eq!(sum_of_nth_nominator(100),272);
-    
+    assert_eq!(sum_of_nth_nominator(10), 17);
+    assert_eq!(sum_of_nth_nominator(100), 272);
+
 }
